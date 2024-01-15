38cc4e07be668b80836a5f56c96b0e21fe7fd9db
Maya <maya.camps@estudiantat.upc.edu>
2024-01-15 13:16:29 +0100

    version control

--- src/main/java/cat/itacademy/barcelonactiva/camps/maya/s04/t02/n01/controllers/FruitController.java
+++ src/main/java/cat/itacademy/barcelonactiva/camps/maya/s04/t02/n01/controllers/FruitController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/fruit")
+@RequestMapping("/fruit/v1")
 public class FruitController {
     @Autowired
     FruitServiceImplementation fruitServiceImp;
